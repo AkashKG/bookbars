19cbee646668b713f7b3ac44b7bf743bd0863f91
Akash Gupta <yesitsakash@gmail.com>
2016-02-14 23:39:18 +0530

    test for merging branches at different position in a network
    Change-Id: I12576442efc86adf65ebff8c33a9276e9d6b1477
    Signed-off-by: Akash Gupta <yesitsakash@gmail.com>

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -93,8 +93,4 @@ angular.module('MainCtrl', []).controller(
 	};
 });
 
-/**
- * Copyright 2016 Google Inc. All Rights Reserved. Use of this source code is
- * governed by an MIT-style license that can be in foundin the LICENSE file at
- * http://material.angularjs.org/license.
- */
+
