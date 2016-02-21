102cc975bd107b02f85fc837266ecf39d2c7c593
Tushar Sinha <tushar.sinha313@gmail.com>
2016-02-21 19:44:45 +0530

    ContactUs completed.
    Change-Id: I78f66671ca12ce6cdcaa6f7e96b4a57aa762a94a
    Signed-off-by: Tushar Sinha <tushar.sinha313@gmail.com>

--- public/js/controllers/ContactCtrl.js
+++ public/js/controllers/ContactCtrl.js
@@ -1,5 +1,18 @@
 angular.module('ContactCtrl', []).controller('ContactController', function($scope) {
 
-	$scope.tagline = 'The square root of life is pi!';	
+	$scope.contact =[{
+		icon : 'phone',
+		type : 'Phone',
+		detail : '+91XXXXXXXXXX'
+		}, {
+		icon : 'home',
+		type : 'Address',
+		detail : 'KIIT University,Patia,Bhubanesheawar,India.'	
+		}, {
+		icon : 'email',
+		type : 'Email',
+		detail : 'bookbaterindia@gmail.com'				
+	}
+	];
 
 });
