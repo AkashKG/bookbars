82c9af15b3add85dd5359f7348debd7d0de07133
Atul Gupta <atulgupta9@gmail.com>
2016-02-21 14:12:34 +0530

    Facebook Authentication done
    Change-Id: Ie40f51941eaa21a6d99e2f575d42a60d926eb2bc
    Signed-off-by: Atul Gupta <atulgupta9@gmail.com>

--- auth.js
+++ auth.js
@@ -9,16 +9,17 @@ function setupAuth(User, app) {
 		done(null,user._id);
 	});
 	passport.deserializeUser(function(id,done){
-		user.findOne({_id:id}).exec(done);
+		User.findOne({_id:id}).exec(done);
 	});
 	passport.use(new FacebookStrategy({
 		clientID : '1587108738178207',
 		clientSecret : '08180a72253539094e531b53be914c96',
-		callbackURL : "http://localhost:8181/auth/facebook/callback"
+		callbackURL : "http://localhost:8181/auth/facebook/callback",
+		"profileFields": ["id","email"]
 	}, function(accessToken, refreshToken, profile, done) {
-		if(!profile.emails || !profile.emails.length){
+		/*if(!profile.emails || !profile.emails.length){
 			return done('No emails associated with this account. . .');
-		}
+		}*/
 		User.findOneAndUpdate(
 				{'data.oauth':profile.id},
 				{
@@ -43,13 +44,13 @@ function setupAuth(User, app) {
 
 	  // Express routes for auth
 	  app.get('/auth/facebook',
-	    passport.authenticate('facebook', { scope: ['email'] }));
+			  passport.authenticate('facebook', { scope: ['email'] })),
 
 	  app.get('/auth/facebook/callback',
 	    passport.authenticate('facebook', { failureRedirect: '/fail' }),
 	    function(req, res) {
 	      res.send('Welcome, ' + req.user.profile.username);
-	    });
+	    })
 }
 
 module.exports = setupAuth;
