4d56962471c2cd9c4cee81ef24c76467a1ef9727
Akash Gupta <yesitsakash@gmail.com>
2016-02-23 19:36:22 +0530

    DB schema modified, local strategy added for atul to continue.
    Change-Id: I146a5747f7b780d6c4bae14b0ad72fc4daa01f9f
    Signed-off-by: Akash Gupta <yesitsakash@gmail.com>

--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,6 @@
 // grab the mongoose module
 var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
 
 // module.exports allows us to pass this to other files when it is called
 var userSchema =  {
@@ -51,8 +52,21 @@ var userSchema =  {
 				min:1
 			}	
 		}]
+	},
+	dataLocal:{
+		email:{type:String, required:true},
+		password:{type:String, required:true}
 	}
 };
 
+
+
 module.exports = new mongoose.Schema(userSchema);
+module.exports.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+module.exports.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.dataLocal.password);
+};
 module.exports.userSchema = userSchema;
+
--- auth.js
+++ auth.js
@@ -4,53 +4,126 @@
 function setupAuth(User, app) {
 	var passport = require('passport');
 	var FacebookStrategy = require('passport-facebook').Strategy;
+	var LocalStrategy = require('passport-local').Strategy;
 
-	passport.serializeUser(function(user,done){
-		done(null,user._id);
+	passport.serializeUser(function(user, done) {
+		done(null, user._id);
 	});
-	passport.deserializeUser(function(id,done){
-		User.findOne({_id:id}).exec(done);
+	passport.deserializeUser(function(id, done) {
+		User.findOne({
+			_id : id
+		}).exec(done);
 	});
+	// =========================================================================
+	// LOCAL SIGNUP ============================================================
+	// =========================================================================
+	// we are using named strategies since we have one for login and one for
+	// signup
+	// by default, if there was no name, it would just be called 'local'
+
+	passport.use('local-signup', new LocalStrategy({
+		// by default, local strategy uses username and password, we will
+		// override with email
+		usernameField : 'email',
+		passwordField : 'password',
+		passReqToCallback : true
+	}, function(req, email, password, done) {
+
+		process.nextTick(function() {
+
+			// find a user whose email is the same as the forms email
+			// we are checking to see if the user trying to login already exists
+			User.findOne({
+				'dataLocal.email' : email
+			},
+					function(err, user) {
+						// if there are any errors, return the error
+						if (err)
+							return done(err);
+
+						// check to see if theres already a user with that email
+						if (user) {
+							return done(null, false, req.flash('signupMessage',
+									'That email is already taken.'));
+						} else {
+
+							// if there is no user with that email
+							// create the user
+							var newUser = new User();
+
+							// set the user's local credentials
+							newUser.dataLocal.email = email;
+							newUser.dataLocal.password = newUser
+									.generateHash(password);
+
+							// save the user
+							newUser.save(function(err) {
+								if (err)
+									throw err;
+								return done(null, newUser);
+							});
+						}
+
+					});
+
+		});
+
+	}));
+
 	passport.use(new FacebookStrategy({
 		clientID : '1587108738178207',
 		clientSecret : '08180a72253539094e531b53be914c96',
 		callbackURL : "http://localhost:8181/auth/facebook/callback",
-		"profileFields": ["id","email"]
+		"profileFields" : [ "id", "email" ]
 	}, function(accessToken, refreshToken, profile, done) {
-		if(!profile.emails || !profile.emails.length){
+		if (!profile.emails || !profile.emails.length) {
 			return done('No emails associated with this account. . .');
 		}
-		User.findOneAndUpdate(
-				{'data.oauth':profile.id},
-				{
-					$set:{
-						'profile.username':profile.emails[0].value,
-						'profile.picture':'http://graph.facebook.com/' + profile.id.toString() + '/picture?type=large'
-					}
-				},
-				{'new':true, upsert:true, runValidators:true},
-				function(error,user){
-					done(error,user);
-				});
+		User.findOneAndUpdate({
+			'data.oauth' : profile.id
+		}, {
+			$set : {
+				'profile.username' : profile.emails[0].value,
+				'profile.picture' : 'http://graph.facebook.com/'
+						+ profile.id.toString() + '/picture?type=large'
+			}
+		}, {
+			'new' : true,
+			upsert : true,
+			runValidators : true
+		}, function(error, user) {
+			done(error, user);
+		});
 	}));
-	  // Express middlewares
-	  app.use(require('express-session')({
-	    secret: 'this is a secret',
-	    resave: true,
-	    saveUninitialized: true
-	  }));
-	  app.use(passport.initialize());
-	  app.use(passport.session());
+	// Express middlewares
+	app.use(require('express-session')({
+		secret : 'this is a secret',
+		resave : true,
+		saveUninitialized : true
+	}));
+	app.use(passport.initialize());
+	app.use(passport.session());
 
-	  // Express routes for auth
-	  app.get('/auth/facebook',
-			  passport.authenticate('facebook', { scope: ['email'] })),
+	// Express routes for auth
+	app.get('/auth/facebook', passport.authenticate('facebook', {
+		scope : [ 'email' ]
+	})),
 
-	  app.get('/auth/facebook/callback',
-	    passport.authenticate('facebook', { failureRedirect: '/fail' }),
-	    function(req, res) {
-	      res.send('Welcome <img src ="' + req.user.profile.picture +'"><br>' + req.user.profile.username);
-	    })
+	app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+		failureRedirect : '/fail'
+	}), function(req, res) {
+		res.send('Welcome <img src ="' + req.user.profile.picture + '"><br>'
+				+ req.user.profile.username);
+	}),
+
+	app.get('/connect/local', function(req, res) {
+	});
+	app.post('/connect/local', passport.authenticate('local-signup', {
+		successRedirect : '/profile',
+		failureRedirect : '/connect/local',
+		failureFlash : true
+	}), function(req, res) {
+		res.send('Welcome <img src ="' + req.user.dataLocal.email);
+	});
 }
-
 module.exports = setupAuth;
