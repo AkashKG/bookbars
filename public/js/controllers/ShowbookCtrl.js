2366fa1f1f5f68b3a65b6167bcd36f31beb67c51
Souryadeep <secondary.sd@gmail.com>
2016-03-08 18:13:40 +0530

    Let me know what changes are required

--- public/js/controllers/ShowbookCtrl.js
+++ public/js/controllers/ShowbookCtrl.js
@@ -1,5 +1,10 @@
 angular.module('ShowbookCtrl', []).controller('ShowbookController', function($scope) {
 
 	
-
-});
+                    
+        $scope.items = [
+                        {name:'Heidi',author:'Johanna Spyri',isbn:'123123'},
+                      
+                        ];            
+        
+	});
