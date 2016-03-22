9f5f048fe3d7b6d6c6ef4737e8f88d1aaada78c2
Tushar Sinha <tushar.sinha313@gmail.com>
2016-03-22 18:52:33 +0530

    Help Page Created<Details to be filled>
    Change-Id: Ia5563cc75d15ad4a0fd4e702c3622d01c52c55cd
    Signed-off-by: Tushar Sinha <tushar.sinha313@gmail.com>

--- public/js/controllers/HelpCtrl.js
+++ public/js/controllers/HelpCtrl.js
@@ -1,19 +1,29 @@
-angular.module('HelpCtrl', []).controller('HelpController', function($scope, $rootScope, $http, $location) {
-	/*
-	$http.get('api/v1/me').success(function(data) {
-		$rootScope.userData = data;
-	}).error(function(data) {
-		console.log('Error: ' + data);
-	});
-	*/
-	$scope.faq = [ {
-		name : "How to use book barter?"
+angular.module('HelpCtrl', []).controller('HelpController', function($scope) {
+	
+	$scope.help = [ {
+		head : "Registration/Login",
+		data : "Secondary line text Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam massa quam. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. "
 
-	}, {
-		name : "How to sign in?"
+	},{
+		head : "Book catalogue",
+		data : "Secondary line text Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam massa quam. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. "
 
-	}, {
-		name : "How to request new book?"
+	},{
+		head : "Add/Remove books",
+		data : "Secondary line text Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam massa quam. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. "
 
-	}  ];
+	},{
+		head : "Book Trade",
+		data : "Secondary line text Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam massa quam. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. "
+
+	},{
+		head : "Search Book",
+		data : "Secondary line text Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam massa quam. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. "
+
+	},{
+		head : "Manage Account",
+		data : "Secondary line text Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam massa quam. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. "
+
+	} ];
+
 });
